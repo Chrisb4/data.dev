af5b675815c1dd69c08d61b5f48c76240f5fff0f
Chris Brown <nudgechris@gmail.com>
2017-12-18 23:13:32 -0600

    added db classes and db

---
+++ classes/Config.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Configuration class
+ */
+
+ class Config {
+
+    const DB_HOST = 'localhost',
+          DB_NAME = 'alcatraz',
+          DB_USER = 'cage',
+          DB_PASS = 'Hexagonal1';
+ }
